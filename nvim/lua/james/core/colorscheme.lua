35ce7e7d1f9c136a744d544bd35f898c25edaab1
James Russell <jamesdrus@gmail.com>
2023-01-24 07:05:48 -0800

    Updating neovim config

--- nvim/lua/james/core/colorscheme.lua
+++ nvim/lua/james/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(vim.cmd, "colorscheme vscode")
+local status, _ = pcall(vim.cmd, "colorscheme tokyonight")
 if not status then
     print("Colorscheme not found")
     return
