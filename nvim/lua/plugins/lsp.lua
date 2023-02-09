19f0bf981d648c286e5383d059071ca4f825f7d2
James Russell <jamesdrus@gmail.com>
2023-02-08 19:47:45 -0800

    Updating nvim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,7 +11,8 @@ return {
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "rust_analyzer"
+                    "rust_analyzer",
+                    "sumneko_lua"
                 }
             })
         end,
@@ -20,27 +21,37 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            require("lspconfig")["rust_analyzer"].setup({
-                capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                on_attach = function(client, bufnr)
-                    local opts = { noremap = true, silent = true, buffer = bufnr }
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                    vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
-                    vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-                    vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
-                    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-                    vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
-                    vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
-                    vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-                    vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
-                    vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-                    vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-                    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-                    vim.keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
-                end,
+            local on_attach = function(client, bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+
+                vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
+                vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+                vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+                vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+                vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+                vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+                vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+                vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+                vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+                vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+                vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+                vim.keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+            end
+
+            require("lspconfig")["rust_analyzer"].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
                 settings = {
                     ["rust-analyzer"] = {}
                 }
+            })
+
+            require("lspconfig")["sumneko_lua"].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {}
             })
         end,
         enabled = true
