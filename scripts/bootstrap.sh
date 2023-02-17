b18986296317b01d27593b08b69f3ec9b3ed8192
James Russell <jamesdrus@gmail.com>
2023-02-16 23:27:24 -0800

    Updating bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,23 +4,28 @@ dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 dnf install -y akmod-nvidia
 
 dnf install -y fedora-workstation-repositories
-dnf config-manager --sfet-enabled google-chrome
+dnf config-manager --set-enabled google-chrome
 dnf copr enable atim/lazygit -y
 
 dnf install -y alacritty neovim bspwm sxhkd picom xsetroot @base-x google-chrome-stable \
                 xrandr ripgrep fd-find fzf lazygit gcc-c++ nodejs bash-completion playerctl \
                 dbus-x11 xsecurelock xset xss-lock
 
-ln -sf ~/projects/dotfiles/bash/bashrc ~./.bashrc
-ln -sf ~/projects/dotfiles/xorg/xinitrc ~/.xinitrc
-ln -sf ~/projects/dotfiles/fonts ~/.fonts
-ln -sf ~/projects/dotfiles/git/gitconfig ~/.gitconfig
-ln -sf ~/projects/dotfiles/alacritty ~/.config/alacritty
-ln -sf ~/projects/dotfiles/bspwm ~/.config/bspwm
-ln -sf ~/projects/dotfiles/sxhkd ~/.config/sxhkd
-ln -sf ~/projects/dotfiles/picom ~/.config/picom
-ln -sf ~/projects/dotfiles/nvim ~/.config/nvim
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+
+mkdir -p $USER_HOME/.config
+mkdir -p $USER_HOME/.config/lazygit
+
+ln -sf $USER_HOME/projects/dotfiles/bash/bashrc $USER_HOME/.bashrc
+ln -sf $USER_HOME/projects/dotfiles/xorg/xinitrc $USER_HOME/.xinitrc
+ln -sf $USER_HOME/projects/dotfiles/fonts $USER_HOME/.fonts
+ln -sf $USER_HOME/projects/dotfiles/git/gitconfig $USER_HOME/.gitconfig
+ln -sf $USER_HOME/projects/dotfiles/alacritty $USER_HOME/.config/alacritty
+ln -sf $USER_HOME/projects/dotfiles/bspwm $USER_HOME/.config/bspwm
+ln -sf $USER_HOME/projects/dotfiles/sxhkd $USER_HOME/.config/sxhkd
+ln -sf $USER_HOME/projects/dotfiles/picom $USER_HOME/.config/picom
+ln -sf $USER_HOME/projects/dotfiles/nvim $USER_HOME/.config/nvim
 ln -sf /usr/bin/nvim /usr/local/bin/vim
-ln -sf ~/projects/dotfiles/xorg/10-mouse.conf /etc/X11/xorg.conf.d/10-mouse.conf
-ln -sf ~/projects/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
-ln -sf ~/projects/dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf $USER_HOME/projects/dotfiles/xorg/10-mouse.conf /etc/X11/xorg.conf.d/10-mouse.conf
+ln -sf $USER_HOME/projects/dotfiles/lazygit/config.yml $USER_HOME/.config/lazygit/config.yml
+ln -sf $USER_HOME/projects/dotfiles/bash/bash_profile $USER_HOME/.bash_profile
