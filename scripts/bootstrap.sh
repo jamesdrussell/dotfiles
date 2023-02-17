f7fc5f71a936818bd23172972593ae60fc46cb00
James Russell <jamesdrus@gmail.com>
2023-02-17 00:10:02 -0800

    Updating bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ dnf copr enable atim/lazygit -y
 
 dnf install -y alacritty neovim bspwm sxhkd picom xsetroot @base-x google-chrome-stable \
                 xrandr ripgrep fd-find fzf lazygit gcc-c++ nodejs bash-completion playerctl \
-                dbus-x11 xsecurelock xset xss-lock
+                dbus-x11 xsecurelock xset xss-lock neofetch
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
