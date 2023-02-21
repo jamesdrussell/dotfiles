01e50c048e5be64227b23c87fa569012e2152b54
James Russell <jamesdrus@gmail.com>
2023-02-21 04:46:22 -0800

    Updating bspwm config and bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,21 +2,21 @@
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-vaapi-driver
-systemctl disable nvidia-powerd.service
+#dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-vaapi-driver
+#systemctl disable nvidia-powerd.service
 
 sudo dnf groupupdate -y core
 sudo dnf groupupdate -y base-x
 sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate -y sound-and-video
 
-dnf install -y fedora-workstation-repositories
-dnf config-manager --set-enabled google-chrome
+#dnf install -y fedora-workstation-repositories
+#dnf config-manager --set-enabled google-chrome
 dnf copr enable atim/lazygit -y
 
-dnf install -y alacritty neovim bspwm sxhkd picom xsetroot google-chrome-stable \
+dnf install -y alacritty neovim bspwm sxhkd picom xsetroot chromium \
                 xrandr ripgrep fd-find fzf lazygit gcc-c++ nodejs bash-completion playerctl \
-                dbus-x11 xsecurelock xset xss-lock neofetch emacs
+                dbus-x11 xsecurelock xset xss-lock neofetch
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
