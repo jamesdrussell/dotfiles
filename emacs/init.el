abf128a6fff48c1126d6f5434178ee8a2d638931
James Russell <jamesdrus@gmail.com>
2023-06-07 13:27:46 -0700

    Updating emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,37 @@
+(load-theme 'modus-vivendi t)
+
 (setq ring-bell-function 'ignore)
 (tool-bar-mode -1)
 (set-frame-font "Menlo 15" nil t)
 (setq vc-follow-symlinks t)
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7887cf8b470098657395502e16809523b629249060d61607c2225d2ef2ad59f5" "cca1d386d4a3f645c2f8c49266e3eb9ee14cf69939141e3deb9dfd50ccaada79" default))
+ '(package-selected-packages '(evil modus-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
