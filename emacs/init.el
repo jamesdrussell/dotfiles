0a5b6c2569e4f8f4cc8108e1b58abc312f266ef7
James Russell <jamesdrus@gmail.com>
2023-07-25 10:23:01 -0700

    Updating emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -45,6 +45,14 @@
   :ensure t
   :init (doom-modeline-mode 1))
 
+(use-package nerd-icons
+  ;; :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
+
 (use-package evil
   :ensure t
   :init
