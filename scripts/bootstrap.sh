ba484a596f4d53f1085f386e8f5c50cccd1dd13b
James Russell <jamesdrus@gmail.com>
2023-07-27 14:27:00 -0700

    Updating bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,7 +16,8 @@ dnf copr enable atim/lazygit -y
 
 dnf install -y alacritty neovim bspwm sxhkd picom xsetroot google-chrome-stable \
                 xrandr ripgrep fd-find fzf lazygit gcc-c++ nodejs bash-completion \
-                playerctl dbus-x11 xsecurelock xset xss-lock flatpak dmenu
+                playerctl dbus-x11 xsecurelock xset xss-lock flatpak dmenu \
+                google-noto-emoji-color-fonts tmux pulseaudio-utils emacs
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
