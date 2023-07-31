610890b4cf5273fa8f849109bd918e1689be9621
James Russell <jamesdrus@gmail.com>
2023-07-31 12:11:26 -0700

    Updating emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -42,7 +42,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one-light t)
+  (load-theme 'doom-one t)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
