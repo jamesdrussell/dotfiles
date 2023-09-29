15e56270adf4e44bbf5cd61477170824e07a5925
James Russell <jamesdrus@gmail.com>
2023-09-28 19:49:24 -0700

    Updating nvim config

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,7 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-        require("indent_blankline").setup()
+        require("ibl").setup()
     end,
     enabled = true
 }
