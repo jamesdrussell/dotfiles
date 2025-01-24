e26326c36f3a2bce3c66c0659a99beed1263f727
James Russell <jamesdrus@gmail.com>
2025-01-24 06:00:22 -0800

    Updating wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 12.0
 config.audible_bell = "Disabled"
 config.automatically_reload_config = true
 return config
