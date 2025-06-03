5056e55f23652cc95cf0145f78be5140275e58bc
James Russell <jamesdrus@gmail.com>
2025-06-02 20:37:02 -0700

    Updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -86,7 +86,7 @@ require('lazy').setup({
       build = ':TSUpdate',
       config = function()
 	      require('nvim-treesitter.configs').setup({
-  	      ensure_installed = { 'c', 'cpp', 'lua' },
+  	      ensure_installed = { 'c', 'cpp', 'lua', 'markdown', 'markdown_inline' },
   	      sync_install = false,
   	      auto_install = true,
   	      highlight = { enable = true },
