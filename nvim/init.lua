c4652ae133956676d8624f2d3c5138e96e14625f
James Russell <jamesdrus@gmail.com>
2025-06-02 18:15:35 -0700

    Updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,8 +30,25 @@ require('lazy').setup({
     {
       'nvim-treesitter/nvim-treesitter',
       lazy = false,
-      branch = 'main',
-      build = ':TSUpdate'
+      build = ':TSUpdate',
+      config = function()
+	      require('nvim-treesitter.configs').setup({
+  	      ensure_installed = { 'c', 'cpp', 'lua' },
+  	      sync_install = false,
+  	      auto_install = true,
+  	      highlight = { enable = true },
+  	      indent = { enable = false },
+      		incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = 'gnn',
+              node_incremental = 'grn',
+              scope_incremental = 'grc',
+              node_decremental = 'grm'
+            }
+          }
+        })
+      end
     },
     {
       'ibhagwan/fzf-lua',
