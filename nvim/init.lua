6ae61fb99395776da66283b169ae11bd334a71c9
James Russell <jamesdrus@gmail.com>
2025-07-06 13:36:24 -0700

    Updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -188,12 +188,6 @@ require('lazy').setup({
       end
     },
     {
-      'sindrets/diffview.nvim',
-      config = function()
-        require('diffview').setup()
-      end
-    },
-    {
       'kylechui/nvim-surround',
       config = function()
         require('nvim-surround').setup()
