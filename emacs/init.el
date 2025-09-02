3611c25ad3ca4808fe4f24dfbfdd58ccf884397f
James Russell <jamesdrus@gmail.com>
2025-09-01 22:40:06 -0700

    Updating emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -11,7 +11,7 @@
     (scroll-bar-mode -1)
 		(setq-default header-line-format '(""))
 		(set-face-background 'header-line (face-background 'default))
-    (set-default mode-line-format '(" %b"))
+    (setq-default mode-line-format '(" %b"))
 		(set-face-background 'mode-line (face-background 'default))))
 
 (vertico-mode)
