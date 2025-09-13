670a83877738083da30888c740cff771cd8c9a32
James Russell <jamesdrus@gmail.com>
2025-09-13 16:02:42 -0700

    Updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,6 +72,10 @@ vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-k>", "<Up>")
 vim.keymap.set("i", "<C-l>", "<Right>")
 vim.keymap.set("n", "U", "<C-r>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 require('lazy').setup({
   spec = {
