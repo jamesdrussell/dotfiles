03b00a5924321ab5ec6c2793ce4265127b1b8e7e
James Russell <jamesdrus@gmail.com>
2025-09-21 15:30:57 -0700

    Updating home-manager config

--- home-manager/pc/flake.nix
+++ home-manager/pc/flake.nix
@@ -1,23 +1,20 @@
 {
-  description = "My Home Manager flake";
+  description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use a specific branch like nixos-25.05 or nixos-unstable
-
+    # Increment release branch for NixOS
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager"; # Specify the desired release or branch
-      inputs.nixpkgs.follows = "nixpkgs"; # Ensure home-manager uses the same nixpkgs version
+      # Follow corresponding `release` branch from Home Manager
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     homeConfigurations."james" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs;
-
-      # Import your home.nix configuration file
-      modules = [
-        ./home.nix
-      ];
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
     };
   };
 }
