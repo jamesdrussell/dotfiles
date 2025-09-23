1124735c2b316e0622fa78c55789306284ed6e95
James Russell <jamesdrus@gmail.com>
2025-09-23 13:27:26 -0700

    Updating nix config

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -47,40 +47,6 @@
     };
   };
 
-#    systemd.services."gnome-suspend" = {
-#      description = "suspend gnome shell";
-#      before = [
-#        "systemd-suspend.service"
-#        "systemd-hibernate.service"
-#        "nvidia-suspend.service"
-#        "nvidia-hibernate.service"
-#      ];
-#      wantedBy = [
-#        "systemd-suspend.service"
-#        "systemd-hibernate.service"
-#      ];
-#      serviceConfig = {
-#        Type = "oneshot";
-#        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
-#      };
-#    };
-#    systemd.services."gnome-resume" = {
-#      description = "resume gnome shell";
-#      after = [
-#        "systemd-suspend.service"
-#        "systemd-hibernate.service"
-#        "nvidia-resume.service"
-#      ];
-#      wantedBy = [
-#        "systemd-suspend.service"
-#        "systemd-hibernate.service"
-#      ];
-#      serviceConfig = {
-#        Type = "oneshot";
-#        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
-#      };
-#    };
-
   hardware.graphics = {
     enable = true;
   };
@@ -149,13 +115,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  #services.displayManager.gdm = {
-  #  enable = true;
-  #  #autoSuspend = false;
-  #};
-  # services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -230,9 +189,6 @@
   };
 
   security.polkit.enable = true;
-
-  # Install firefox.
-  #programs.firefox.enable = true;
 
   programs._1password.enable = true;
   programs._1password-gui.enable = true;
