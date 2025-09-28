d27af432c2c3639135bd6bd8bcff99e7e864d79d
James Russell <jamesdrus@gmail.com>
2025-09-27 19:08:43 -0700

    Updating nix config

--- nixos/vm/configuration.nix
+++ nixos/vm/configuration.nix
@@ -120,7 +120,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PrintLastLog = false;
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
